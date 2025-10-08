2927a5c6c23af6316ce08d949babc3f8c5d29740
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2025-10-07 17:32:33 -0700

    Base path + deploy-ready

--- src/components/EventsGraphic.tsx
+++ src/components/EventsGraphic.tsx
@@ -6,7 +6,7 @@ import html2canvas from 'html2canvas'
 function Event ({ data }: { data: ClubEvent }) {
   const { events, setEvents, inputText, setInputText } = useContext(EventsContext);
   const shortName = data.clubName.replace(/\s*(@|at)\s*uci\s*$/gi, '').toLowerCase();
-  const source = `/club-logos/${shortName}.png`;
+  const source = `/icssc-weekly/club-logos/${shortName}.png`;
 
   const handleClick = (event: React.MouseEvent) => {
     if (!event.altKey) return
